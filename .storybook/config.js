3a9dac9b8caef32d2805103608a3435ef4ed6966
zhouwei <zhou21192345@163.com>
2020-04-29 10:07:34 +0800

    taskbox UI

---
+++ .storybook/config.js
@@ -0,0 +1,15 @@
+import { configure } from '@storybook/react';
+import '../src/index.css';
+
+// import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
+// registerRequireContextHook();
+
+// const req = global.__requireContext(__dirname, '../src', true, /\.stories.js$/);
+
+const req = require.context('../src', true, /\.stories.js$/);
+
+function loadStories() {
+  req.keys().forEach((filename) => req(filename));
+}
+
+configure(loadStories, module);
